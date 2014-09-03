af0aaf9c85cd8296396446aa668e4618e8d6d388
Paul Dukes <paul.r.dukes@gmail.com>
2014-09-02 20:57:58 -0400

    Directives
    Moved social media into directive.
    Also with templates inside directives there is no conflict with Liquid
    templates so I removed $interpolateProvider.

--- js/app.js
+++ js/app.js
@@ -3,14 +3,23 @@
 
   var app = angular.module('Blog', []);
 
-  app.config( function($interpolateProvider) {
-    $interpolateProvider.startSymbol('[[');
-    $interpolateProvider.endSymbol(']]');
-  });
 
-  app.controller('SocialLinks', function ($http, $scope) {
-    $http.get('js/social.json').success(function (data) {
-      $scope.socialLinks = data;
-    });
+  // app.config( function($interpolateProvider) {
+  //   $interpolateProvider.startSymbol('[[');
+  //   $interpolateProvider.endSymbol(']]');
+  // });
+
+  app.directive('socialLinks', function () {
+    return {
+      restrict: 'EA',
+      controller: function ($http, $scope) {
+        $http.get('js/social.json').success(function (data) {
+          $scope.socialLinks = data;
+        });
+      },
+      template: '<ul class="social-media"> <li ng-repeat="socialLink in socialLinks" ng-hide="socialLink.hide">\
+      <a href="{{socialLink.url}}" class="large-icon entypo-{{socialLink.class}}" data-icon="{{socialLink.name| lowercase}}" target="_blank">\
+      <span class="visuallyhidden">{{socialLink.name}}<span></a></li></ul>'
+    };
   });
 })();
