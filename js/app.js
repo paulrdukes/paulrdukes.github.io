e8a31692cf201b02a60b514193a5e4815b166ca1
Paul Dukes <paul.r.dukes@gmail.com>
2014-09-16 23:09:25 -0400

    COMMIT ALL THE CHANGES!
    moved more items into directives.  changed file structure. refactored
    styles.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 
   var app = angular.module('Blog', []);
 
-
+  // Prevents conflicts with liquid templating language
   // app.config( function($interpolateProvider) {
   //   $interpolateProvider.startSymbol('[[');
   //   $interpolateProvider.endSymbol(']]');
@@ -11,15 +11,35 @@
 
   app.directive('socialLinks', function () {
     return {
-      restrict: 'EA',
+      restrict: 'EAC',
       controller: function ($http, $scope) {
-        $http.get('js/social.json').success(function (data) {
+        $http.get('data/social.json').success(function (data) {
           $scope.socialLinks = data;
         });
       },
-      template: '<ul class="social-media"> <li ng-repeat="socialLink in socialLinks" ng-hide="socialLink.hide">\
-      <a href="{{socialLink.url}}" class="large-icon entypo-{{socialLink.class}}" data-icon="{{socialLink.name| lowercase}}" target="_blank">\
-      <span class="visuallyhidden">{{socialLink.name}}<span></a></li></ul>'
+      template: '<ul class="social-media"> <li ng-repeat="socialLink in socialLinks" ng-hide="socialLink.hide">' +
+        '<a href="{{socialLink.url}}" class="large-icon entypo-{{socialLink.class}}" data-icon="{{socialLink.name | lowercase}}" target="_blank">' +
+        '<span class="visuallyhidden">{{socialLink.name}}<span></a></li></ul>'
     };
   });
-})();
+
+  app.directive('about', function () {
+    return {
+      restrict: 'EAC',
+      controller: function ($http, $scope) {
+        $http.get('data/about.json').success(function (data) {
+          $scope.about = data;
+        });
+      },
+      // TODO: Clean this up.
+      template:'<h1><a href="/index.html">{{about.title}}</a></h1>'+
+      '<p>{{about.bio}}</p>' +
+      '<social-links></social-links>'+
+      '<img src="{{about.pic}}" alt="profile-pic" class="profile-pic">' +
+      '<h2>Currently</h2>' +
+      '<ul ng-repeat="job in about.jobs">' +
+        '<li><em>{{job.title}}</em> - <a href="{{job.url}}" target="_blank">{{job.company}}</a></li>' +
+      '</ul>'
+    };
+  });
+}());
